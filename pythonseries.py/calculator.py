c619bbe9101f7078413684569af8eb80de944fcf
poojasahu4816 <misspooja.4816@gmail.com>
2025-11-21 07:02:35 +0530

    calculater

---
+++ pythonseries.py/calculator.py
@@ -0,0 +1,21 @@
+expr = input()
+if '+' in expr:
+    A, B = expr.split('+')
+    print(int(A)+int(B))
+elif '-'in expr:
+    A, B = expr.split('-')
+    print(int(A)-int(B))
+elif '*'in expr:
+    A, B = expr.split('*')
+    print(int(A)*int(B))
+elif '/'in expr:
+    A, B = expr.split('/')
+    print(int(A)//int(B))
+    
+    
+
+    
+    
+
+    
+    

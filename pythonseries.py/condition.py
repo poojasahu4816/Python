32fdd8313f1fc5f056dc0e141e8ba60d7325375a
poojasahu4816 <misspooja.4816@gmail.com>
2025-11-21 07:03:59 +0530

    conditions

---
+++ pythonseries.py/condition.py
@@ -0,0 +1,12 @@
+A,S,B = input().split()
+A = int(A)
+S = str(S)
+B = int(B)
+if(S == '+'):
+    print(A + B)
+elif(S == '-'):
+    print(A - B)
+elif(S == '*'):
+    print(A * B)
+elif(S == '/'):
+    print(A/B)

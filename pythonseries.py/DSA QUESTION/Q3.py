d65052fe8850956ca70bf1238a0dc87a8319cfb6
Pooja Sahu <misspooja.4816@gmail.com>
2026-01-06 05:18:16 +0530

    sheet -> Q3

---
+++ pythonseries.py/DSA QUESTION/Q3.py
@@ -0,0 +1,11 @@
+Marks = int(input("Enter your marks: "))
+if(Marks >= 90):
+    print("Grade A")
+elif(Marks >= 80):
+    print("Grade B")
+elif(Marks >= 70):
+    print("Grade C")
+elif(Marks >= 60):
+    print("Grade D")
+else:
+    print("Grade E")

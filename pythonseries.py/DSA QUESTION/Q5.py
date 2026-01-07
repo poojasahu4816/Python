c6dd509e4d41a1dd2465fbf45cc65364f62ac382
Pooja Sahu <misspooja.4816@gmail.com>
2026-01-07 06:01:05 +0530

    sheet -> Q5

---
+++ pythonseries.py/DSA QUESTION/Q5.py
@@ -0,0 +1,9 @@
+Age = int(input("Enter your age....:"))
+if(Age <= 12):
+    print("Child..., Bachhe aap abhi chhote hai...")
+elif(Age <= 19):
+    print("Teenagers -> Inki jindgi ki lagi padi hai ")
+elif(Age <= 64):
+    print("Adult -> Ye thode samajhdar ho chukr hote hai ")
+else:
+    print("Senior -> Kabr me pair...")

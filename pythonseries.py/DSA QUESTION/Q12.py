fed41fbfa5904f31626117776cbe331a0dfa0243
Pooja Sahu <misspooja.4816@gmail.com>
2026-01-11 17:56:07 +0530

    sheet -> Q 12

---
+++ pythonseries.py/DSA QUESTION/Q12.py
@@ -0,0 +1,9 @@
+month = int(input().strip())
+if(3 <= month <= 5):
+    print("Spring...")
+elif(6 <= month <= 8):
+    print("Summer...")
+elif(9 <= month <= 11):
+    print("Autumn...")
+else:
+    print("Winter...")

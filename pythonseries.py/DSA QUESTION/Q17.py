7d6967c4527bd5fec37488c418eaef737089bc64
Pooja Sahu <misspooja.4816@gmail.com>
2026-01-13 07:01:28 +0530

    sheet -> Q 17

---
+++ pythonseries.py/DSA QUESTION/Q17.py
@@ -0,0 +1,18 @@
+num = int(input("Enter Week number...: "))
+
+if(num == 1):
+    print("Monday")
+elif(num == 2):
+    print("Tuesday")
+elif(num == 3):
+    print("Wednesday")
+elif(num == 4):
+    print("Thirsday")
+elif(num == 5):
+    print("Friday")
+elif(num == 6):
+    print("Saturday")
+elif(num == 7):
+    print("Sunday")
+else:
+    print("Invalid choise..")

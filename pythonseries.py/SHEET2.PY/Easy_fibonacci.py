3df817e05231ddf6a5c881eb08b7c9128091d750
Pooja Sahu <misspooja.4816@gmail.com>
2026-01-21 22:51:07 +0530

    sheet -> Q

---
+++ pythonseries.py/SHEET2.PY/Easy_fibonacci.py
@@ -0,0 +1,9 @@
+def fibonacci(n):
+    a, b = 0, 1
+    result = []
+    for i in range(n):
+        result.append(a)
+        a, b =b, a + b
+    return result
+N = int(input().strip())
+print(*fibonacci(N))
